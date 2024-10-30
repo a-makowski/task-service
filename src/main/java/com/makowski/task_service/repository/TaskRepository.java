3e774e0af7e83fefe178c0d69282bc1fa5f08446
Anna Makowski <a.makowski723@gmail.com>
2024-10-31 00:18:10 +0100

    Implement task management methods in TaskService

--- src/main/java/com/makowski/task_service/repository/TaskRepository.java
+++ src/main/java/com/makowski/task_service/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.makowski.task_service.repository;
 import com.makowski.task_service.entity.Task;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends CrudRepository<Task, Long> {
+
+    public List<Task> findByOwner(String owner);
 }
--- src/main/java/com/makowski/task_service/service/TaskService.java
+++ src/main/java/com/makowski/task_service/service/TaskService.java
@@ -1,8 +1,11 @@
 package com.makowski.task_service.service;
 
+import com.makowski.task_service.entity.Task;
 import com.makowski.task_service.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -10,15 +13,53 @@ public class TaskService {
 
     private TaskRepository taskRepository;
 
-    //add task
+    public Task getTaskByID(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(RuntimeException::new);     //TODO change exception
+    }
 
-    //delete task
+    public void ownerCheck(String loggedUser, String taskOwner) {
+        if (!loggedUser.equals(taskOwner)) throw new RuntimeException();  //TODO change exception
+    }
 
-    //rename task
+    public Task addTask(String username, String title) {
+        Task task = new Task();
+        task.setOwner(username);
+        task.setTitle(title);
+        task.setStatus(false);
+        return taskRepository.save(task);
+    }
 
-    //change status
+    public void deleteTask(String username, Long id) {
+        Task task = getTaskByID(id);
+        ownerCheck(username, task.getOwner());
+        taskRepository.deleteById(id);
+    }
 
-    //get task
+    public Task changeTitle(String username, Long id, String title) {
+        Task task = getTaskByID(id);
+        ownerCheck(username, task.getOwner());                      // TODO change exception
+        if (title.isBlank() || title.isEmpty()) throw new RuntimeException();
+        task.setTitle(title);
+        return taskRepository.save(task);
+    }
 
+    public Task changeStatus(String username, Long id) {
+        Task task = getTaskByID(id);
+        ownerCheck(username, task.getOwner());
+        task.setStatus(!task.isStatus());
+        return taskRepository.save(task);
+    }
 
+    public Task getTask(String username, Long id) {
+        Task task = getTaskByID(id);
+        ownerCheck(username, task.getOwner());
+        return task;
+    }
+
+    public List<Task> getMyTasks(String username) {
+        List<Task> tasks = taskRepository.findByOwner(username);
+        if (!tasks.isEmpty()) return tasks;
+            else throw new RuntimeException();  //TODO change exc.
+    }
 }

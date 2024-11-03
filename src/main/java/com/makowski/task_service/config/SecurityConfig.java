9f0a3c7dbd843dca08a3037e08631a6a89e0762a
Anna Makowski <a.makowski723@gmail.com>
2024-11-03 15:49:16 +0100

    Disable X-Frame-Options header in SecurityConfig

--- src/main/java/com/makowski/task_service/config/SecurityConfig.java
+++ src/main/java/com/makowski/task_service/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.makowski.task_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().permitAll()
